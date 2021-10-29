0224e9358b6ec05ebb8befb577fbeaff04ee6c4f
yashtest11111 <91733273+yashtest11111@users.noreply.github.com>
2021-10-29 17:59:07 +0530

    Update repo-info.py

--- repo-info.py
+++ repo-info.py
@@ -2,7 +2,7 @@
 import requests
 
 # defining the api-endpoint
-API_ENDPOINT = "http://115.115.91.60:5432/train"
+API_ENDPOINT =  "http://4533-115-119-250-30.ngrok.io/train"
 # data to be sent to api
 data = {
     "url": "https://github.com/yashtest11111/yash4demo.git",
